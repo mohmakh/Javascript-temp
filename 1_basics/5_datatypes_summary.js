646999dfbc4e1e59819280fb2542adde8d0838db
Mohammad Maaz Akhtar <105709674+mohmakh@users.noreply.github.com>
2024-06-10 05:08:48 +0000

    Added things

--- 1_basics/5_datatypes_summary.js
+++ 1_basics/5_datatypes_summary.js
@@ -52,3 +52,27 @@ Return type of variables in JavaScript
        Function  =>  function
        Object  =>  object
 */
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "maazakhtardotcom"
+
+let anothername = myYoutubename
+anothername = "mma"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "maaz@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
